bc1529e06d2188f5403ab64f6471f1bdafe2e946
Michael Nielsen <lethagicmike@hotmail.com>
2019-10-30 11:40:52 +0100

    Positive transaction implemented.

--- Transactions_Optimistic/UserControl1.Designer.cs
+++ Transactions_Optimistic/UserControl1.Designer.cs
@@ -28,11 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ReadData = new System.Windows.Forms.Button();
+            this.UpdateData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "1001";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // ReadData
+            // 
+            this.ReadData.Location = new System.Drawing.Point(87, 76);
+            this.ReadData.Name = "ReadData";
+            this.ReadData.Size = new System.Drawing.Size(75, 23);
+            this.ReadData.TabIndex = 2;
+            this.ReadData.Text = "ReadData";
+            this.ReadData.UseVisualStyleBackColor = true;
+            this.ReadData.Click += new System.EventHandler(this.ReadData_Click);
+            // 
+            // UpdateData
+            // 
+            this.UpdateData.Location = new System.Drawing.Point(87, 154);
+            this.UpdateData.Name = "UpdateData";
+            this.UpdateData.Size = new System.Drawing.Size(75, 23);
+            this.UpdateData.TabIndex = 3;
+            this.UpdateData.Text = "UpdateData";
+            this.UpdateData.UseVisualStyleBackColor = true;
+            this.UpdateData.Click += new System.EventHandler(this.UpdateData_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Controls.Add(this.UpdateData);
+            this.Controls.Add(this.ReadData);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(471, 446);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button ReadData;
+        private System.Windows.Forms.Button UpdateData;
     }
 }
--- Transactions_Optimistic/UserControl1.cs
+++ Transactions_Optimistic/UserControl1.cs
@@ -1,20 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Transactions_Optimistic
 {
     public partial class UserControl1: UserControl
     {
+        private byte[] rowVersion;
+        private const string username = "user id = dbpractice1001;";
+        private const string server = "server = den1.mssql7.gear.host;";
+        private const string pwd = "password=Fj1XBUQ6Up??";
+        private const string db = "database = dbpractice1001;";
+
         public UserControl1()
         {
             InitializeComponent();
+        }
+
+        private void ReadData_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            using ( SqlConnection conn = new SqlConnection(server + db + username + pwd))
+            {
+                string sqlCmdText = $"Select model, speed, Rowversion " +
+                $"from PC Where " +
+                $"model = @model";
+
+                SqlParameter paramOne = new SqlParameter("@model", SqlDbType.VarChar, 4);
+                paramOne.Value = textBox1.Text;
+                SqlCommand cmd = new SqlCommand(sqlCmdText, conn);
+                cmd.Parameters.Add(paramOne);
+                
+                cmd.Connection.Open();
+                using (SqlDataReader reader  = cmd.ExecuteReader() )
+                {
+                    try {
+                        reader.Read();
+                        rowVersion = (byte[])reader["Rowversion"];
+                        textBox2.Text = reader["speed"].ToString();
+                    }
+                    catch (Exception ex) {
+                        Console.WriteLine(ex.StackTrace);
+                    }
+                    finally {
+                        reader.Close();
+                        cmd.Connection.Close();
+                    }
+                }
+            }
+        }
+
+        private void UpdateData_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(server + db + username + pwd))
+            {
+                string sqlCmdText = $"UPDATE PC " +
+                                    $"SET speed = @speed " +
+                                    $"WHERE model = @model AND Rowversion = @Rowversion";
+
+                SqlParameter paramOne = new SqlParameter("@model", SqlDbType.VarChar, 4);
+                paramOne.Value = textBox1.Text;
+
+                SqlParameter paramTwo = new SqlParameter("@speed", SqlDbType.Int);
+                paramTwo.Value = int.Parse(textBox2.Text);
+
+                SqlParameter paramThree = new SqlParameter("@Rowversion", SqlDbType.Timestamp);
+                paramThree.Value = rowVersion;
+
+
+                SqlCommand cmd = new SqlCommand(sqlCmdText, conn);
+                cmd.Parameters.Add(paramOne);
+                cmd.Parameters.Add(paramTwo);
+                cmd.Parameters.Add(paramThree);
+                cmd.Connection.Open();
+
+                int antal = cmd.ExecuteNonQuery();
+                if (antal > 0)
+                    MessageBox.Show("Success - Changes_Made", "Update_Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("Failure - No_Changes_Made . Timestamp_Has_Changed", "Update_Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                
+                cmd.Connection.Close();
+            }
+
         }
     }
 }
